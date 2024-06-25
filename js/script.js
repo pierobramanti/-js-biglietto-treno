b2c32ab2085988c299249791db7ced17a628107c
pierobramanti <piero.bramanti94@gmail.com>
2024-06-25 16:34:58 +0200

    [UPD] created variable age

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 // CREO LA VARIABILE KM //
 let km =prompt("Quanti km devi percorrere per arrivare a destinazione?")
 console.log(km)
+
+// CREO LA VARIABILE AGE//
+let age=prompt("Quanti anni hai?")
+console.log(age)
