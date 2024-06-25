c92683e37d2a6e370fa2825afd072385b3c3b7c0
pierobramanti <piero.bramanti94@gmail.com>
2024-06-25 16:41:32 +0200

    [UPD] assigned value to variable cost_km

--- js/script.js
+++ js/script.js
@@ -5,3 +5,7 @@ console.log(km)
 // CREO LA VARIABILE AGE//
 let age=prompt("Quanti anni hai?")
 console.log(age)
+
+// CREO LA VARIABILE COST_KM E LE ASSEGNO IL VALORE (0.21)//
+let cost_km = 0.21;
+console.log(cost_km)
